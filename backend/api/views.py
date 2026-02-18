cc658bc309c38eba45b5d0cb7577cb2e33ab60db
mushie-mush <66895377+mushie-mush@users.noreply.github.com>
2026-02-18 13:47:43 +0800

    feat(backend): add login and register user endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, views
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+class UserLoginView(views.APIView):
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        return Response({'error': 'Invalid credentials'}, status=400)
