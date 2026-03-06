fc83ed5c7409d5a1268c3e6a6d3e79e1013f5771
mushie-mush <66895377+mushie-mush@users.noreply.github.com>
2026-03-06 23:26:13 +0800

    refactor(frontend): rename IColumn & ITask property names

--- frontend/src/features/board/board.d.ts
+++ frontend/src/features/board/board.d.ts
@@ -9,14 +9,14 @@ interface IColumn {
   id: string;
   title: string;
   description?: string;
-  boardId: string;
+  board: string;
 }
 
 interface ITask {
   id: string;
   title: string;
   description?: string;
-  columnId: string;
+  column: string;
 }
 
 export type { IBoard, IColumn, ITask };
